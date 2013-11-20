55608486c14962bbac5a104617ea4e1458605689
pechi169@gmail.com <pechi169@gmail.com@a0ddf5f0-f1a4-2ebb-8d89-1eaa78eac3b4>
2013-11-20 11:39:50 +0000

    missing files

---
+++ trunk/BloodStoneProject/WarehouseSystem/Store.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WarehouseSystem
+{
+    public abstract class Store
+    {
+        protected List<StoreObject> productsList = new List<StoreObject>();
+        protected List<string> productCategories = new List<string>();
+
+        public Store()
+        {
+            LoadStore();
+        }
+
+        public void AddProduct(StoreObject product)
+        {
+            this.productsList.Add(product);
+        }
+
+        public List<StoreObject> GetAllProducts()
+        {
+            return this.productsList;
+        }
+
+        public List<string> GetCategories
+        {
+            get
+            {
+                return this.productCategories;
+            }
+        }
+
+        public abstract void LoadStore();
+    }
+}

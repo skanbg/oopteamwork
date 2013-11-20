77cc624260e16918a2b778c3ba02fa80a5fbc48c
pechi169@gmail.com <pechi169@gmail.com@a0ddf5f0-f1a4-2ebb-8d89-1eaa78eac3b4>
2013-11-20 11:39:50 +0000

    missing files

---
+++ BloodStoneProject/WarehouseSystem/DesktopStore.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace WarehouseSystem
+{
+    public class DesktopStore : Store
+    {
+        public override void LoadStore()
+        {
+            //Loading product categories from file
+            var categoriesReader = new StreamReader("ProductCategories.txt");
+            var roughCategoriesData = categoriesReader.ReadToEnd();
+            var splitedCategories = roughCategoriesData.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var category in splitedCategories)
+            {
+                this.productCategories.Add(category);
+            }
+
+            //Loading products from file
+            var productReader = new StreamReader("ProductList.txt");
+            var roughProductsData = productReader.ReadToEnd();
+            //Each product on new line
+            var splitedProducts = roughProductsData.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                foreach (var productToken in splitedProducts)
+                {
+                    //Splitting product type from product properties
+                    var splitedProductProperties = productToken.Split(new string[] { " ~obj~ " }, StringSplitOptions.RemoveEmptyEntries);
+                    Type classType = Type.GetType("WarehouseSystem." + splitedProductProperties[0].Trim(), true);
+                    var userSettings = classType;
+                    //Raboti i s dolnoto
+                    //var userSettings = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(type => type.Name == splitedProductProperties[0]);
+                    var currentSettings = Activator.CreateInstance(userSettings);
+                    splitedProductProperties = splitedProductProperties[1].Split(new string[] { " ~|~ " }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var productProperty in splitedProductProperties)
+                    {
+                        var propertyInfos = productProperty.Split(new string[] { "~~" }, StringSplitOptions.RemoveEmptyEntries);
+                        var property = userSettings.GetProperty(propertyInfos[0]);
+                        var type = property.PropertyType;
+                        dynamic newValue;
+                        if(type.Name=="Branch")
+                        {
+                            newValue = (Branch)int.Parse(propertyInfos[1]);
+                        }
+                        else
+                        {
+                            newValue = Convert.ChangeType(propertyInfos[1], type);
+                        }
+                        property.SetValue(currentSettings, newValue);
+                        
+                        var userSettingsTypeProperties = currentSettings.GetType().GetProperty(propertyInfos[0]).GetValue(currentSettings, null).ToString();
+                        //System.Windows.MessageBox.Show(userSettingsTypeProperties);
+
+
+                        ////PropertyInfo prop = classType.GetProperty(propertyInfos[0]);
+                        //var prop = classType.GetType().GetProperty(propertyInfos[0]);
+                        //System.Windows.MessageBox.Show(propertyInfos[0]);
+
+                        ////////var property = classType.GetProperty(propertyInfos[0]);
+                        ////////var type = property.PropertyType;
+                        ////////var newValue = Convert.ChangeType(propertyInfos[1], type);
+                        ////////property.SetValue(product, newValue);
+
+
+                        //////////prop.SetValue(product, propertyInfos[1], null);
+                        //////////product.GetType().GetProperty(propertyInfos[0]).SetValue(product, propertyInfos[1], null);
+                    }
+                    this.AddProduct(currentSettings as StoreObject);
+                    //this.productCategories.Add(category);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message);
+            }
+            //System.Windows.MessageBox.Show(this.productsList.Count.ToString());
+            //System.Windows.MessageBox.Show(this.productsList[2].Model);
+        }
+    }
+}
